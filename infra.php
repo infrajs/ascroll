4e711cfa3c52297526d9a91f690e63a6e138b558
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-12 14:12:08 +0400

    namespaces

--- infra.php
+++ infra.php
@@ -4,8 +4,7 @@ use infrajs\event\Event;
 use infrajs\view\View;
 use infrajs\infra\Infra;
 
-$conf=Infra::config('ascroll');
-if (!$conf['ascroll']) return;
+
 Event::handler('onjs', function () {	
 	View::js('*ascroll/ascroll.js');
 	View::js('*ascroll/infra.js');
